92215de72459560c5916d8909acaecdd96fe09db
Soldret <soldret@acd41229045323768737381.wireless.rit.edu>
2026-01-30 11:50:57 -0500

    Rudimentary File Reader in C with simple O(n) search

--- tools/fat/fat.c
+++ tools/fat/fat.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef uint8_t bool;
 #define true 1
 #define false 0
+//FNAME is the file name size in Dir Entry Specification, similar for FEXT = File Ext
+#define FNAME_SIZE 8
+#define FEXT_SIZE 3
 
 typedef struct {
 	uint8_t BootJumpInstruction[3];
@@ -29,8 +34,25 @@ typedef struct {
 	uint8_t SystemId[8];
 } __attribute__((packed)) BootSector;
 
+typedef struct {
+	uint8_t name[8];
+	uint8_t ext[3];
+	uint8_t attr;
+	uint8_t __reserved;
+	uint8_t CreateTimeFine;
+	uint16_t CreationTime;
+	uint16_t CreationDate;
+	uint16_t LastAccess;
+	uint16_t EAIndex;
+	uint16_t LastModTime;
+	uint16_t LastModDate;
+	uint16_t FirstCluster;
+	uint32_t FileSize;
+} __attribute__((packed)) DirectoryEntry;
+
 BootSector g_BS;
-uint8_t g_FAT = NULL;
+uint8_t* g_FAT = NULL;
+DirectoryEntry* g_RootDir = NULL;
 
 bool readBootSector(FILE* disk) {
 	return fread(&g_BS, sizeof(g_BS), 1, disk) > 0;
@@ -48,6 +70,26 @@ bool readFAT(FILE* disk) {
 	return readSectors(disk, g_BS.ReservedSectors, g_BS.SectorsPerFat, g_FAT);
 }
 
+bool readRootDirectory(FILE* disk) {
+	uint32_t lba = g_BS.ReservedSectors + g_BS.SectorsPerFat * g_BS.FatCount;
+	uint32_t size = sizeof(DirectoryEntry)*g_BS.DirEntryCount;
+	uint32_t sectors = (size / g_BS.BytesPerSector);
+	if(size % g_BS.BytesPerSector > 0) {
+		sectors++;
+	}
+	g_RootDir = (DirectoryEntry*) malloc(sectors * g_BS.BytesPerSector);
+	return readSectors(disk, lba, sectors, g_RootDir);
+}
+
+DirectoryEntry* findFile(const char* name) {
+	for(uint32_t i = 0; i < g_BS.DirEntryCount; i++) {
+		if(memcmp(name, g_RootDir[i].name, 11) == 0) {
+			return &g_RootDir[i];
+		}
+	}
+	return NULL;
+}
+
 int main (int argc, char** argv) {
 	if (argc < 3) {
 		printf("Syntax: %s <disk image> <file name>\n", argv[0]);
@@ -63,5 +105,25 @@ int main (int argc, char** argv) {
 		fprintf(stderr, "Could not read boot sector\n");
 		return -2;
 	}
+	if(!readFAT(disk)) {
+		fprintf(stderr, "Could not read FAT\n");
+		free(g_FAT);
+		return -3;
+	}
+	if(!readRootDirectory(disk)) {
+		fprintf(stderr, "Error Reading Root Dir\n");
+		free(g_FAT);
+		free(g_RootDir);
+		return -4;
+	}
+	DirectoryEntry* fileEntry = findFile(argv[2]);
+	if(!fileEntry) {
+		fprintf(stderr, "Could not find file %s\n", argv[2]);
+		free(g_FAT);
+		free(g_RootDir);
+		return -5;
+	}
+	free(g_FAT);
+	free(g_RootDir);
 	return 0;
 }
